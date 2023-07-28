3beb550afba294f77847c53037de729dc6d1ea02
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2023-07-28 23:41:35 +0300

    add input

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.innerBox}>
-        <Text>Hallo ;)</Text>
+        <Text style={styles.text}>Hallo ;)</Text>
+        <TextInput style={styles.button} textAlign={"center"}></TextInput>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -18,13 +19,29 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "silver",
   },
   innerBox: {
     borderWidth: 1,
-    borderColor: "red",
+    borderColor: "black",
     padding: 20,
     paddingLeft: 40,
     paddingRight: 40,
     borderRadius: 6,
+    borderWidth: 2,
+    backgroundColor: "tomato",
+    width: 300,
+  },
+  text: {
+    fontSize: 30,
+    textAlign: "center",
+  },
+  button: {
+    borderRadius: 6,
+    backgroundColor: "white",
+    color: "white",
+    fontSize: 20,
+    // textAlign: "center",
+    marginHorizontal: 20,
   },
 });
