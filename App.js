b694ceea31b186f84df90a2412af6ac328e415e8
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2023-08-13 00:04:28 +0300

    Add some logic

--- App.js
+++ App.js
@@ -1,32 +1,79 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
+  Button,
   ImageBackground,
   StyleSheet,
   Text,
   TextInput,
   View,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
 } from "react-native";
 
 export default function App() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  // console.log(login);
+  // console.log(password);
+
+  const data = {
+    login,
+    password,
+  };
+  console.log(data);
+
+  const inputLogin = (text) => setLogin(text);
+  const inputPassword = (text) => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert("Собрал перед отправкой", `${login} ${password}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require("./images/1.jpg")} style={styles.image}>
-        <View style={styles.innerBox}>
-          <Text style={styles.text}>Реєстрація</Text>
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder="Login"
-          ></TextInput>
-          <TextInput
-            style={styles.input}
-            textAlign={"center"}
-            placeholder="Password"
-          ></TextInput>
-        </View>
-        <StatusBar style="auto" />
-      </ImageBackground>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <ImageBackground
+            source={require("./images/1.png")}
+            style={styles.image}
+          >
+            <View style={styles.innerBox}>
+              <Text style={styles.text}>Реєстрація</Text>
+              <TextInput
+                value={login}
+                style={styles.input}
+                placeholder="Логін"
+                onChangeText={inputLogin}
+              ></TextInput>
+              <TextInput
+                style={styles.input}
+                placeholder="Адреса електронної пошти"
+              ></TextInput>
+              <TextInput
+                value={password}
+                style={styles.input}
+                placeholder="Пароль"
+                onChangeText={inputPassword}
+              ></TextInput>
+              <Button
+                title="Зарееструватися"
+                color="#FF6C00"
+                accessibilityLabel="Learn more about this purple button"
+                // onPress={() => Alert.alert("Simple Button pressed")}
+                onPress={onLogin}
+              />
+            </View>
+            <StatusBar style="auto" />
+          </ImageBackground>
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -44,10 +91,10 @@ const styles = StyleSheet.create({
     padding: 20,
     paddingLeft: 40,
     paddingRight: 40,
-    borderRadius: 6,
-    borderWidth: 2,
-    backgroundColor: "tomato",
-    width: 300,
+    borderRadius: 10,
+    backgroundColor: "white",
+    width: 390,
+    height: 300,
   },
   text: {
     fontSize: 25,
@@ -56,16 +103,24 @@ const styles = StyleSheet.create({
   },
   input: {
     borderRadius: 6,
-    backgroundColor: "white",
-    color: "red",
-    fontSize: 20,
-    // textAlign: "center",
-    marginHorizontal: 20,
+    backgroundColor: "#E8E8E8",
+    width: 315,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
     marginBottom: 10,
+    fontSize: 20,
   },
   image: {
     flex: 1,
     justifyContent: "center",
-    resizeMode: "cover",
+    resizeMode: "stretch",
+    width: 390,
+  },
+  button: {
+    borderColor: "#E8E8E8",
+    borderRadius: 50,
+    backgroundColor: "FF6C00",
   },
 });
