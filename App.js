97972a89fea782ca88bd2bf93ffbfc899327b4a1
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2023-08-22 17:03:33 +0300

    Add login form

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { LoginScrean } from "./screens/loginScreen/LoginScrean";
 import { RegistrationScreen } from "./screens/registrationScreen/RegistrationScreen";
 
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
 
 export default function App() {
-  return <RegistrationScreen />;
+  return <LoginScrean />;
 }
--- screens/loginScreen/LoginScrean.jsx
+++ screens/loginScreen/LoginScrean.jsx
@@ -1,9 +1,52 @@
+import { ImageBackground, TextInput, TouchableOpacity } from "react-native";
 import { View, Text } from "react-native";
+import { styles } from "../registrationScreen/RegistrationScreenStyled";
+import { useState } from "react";
+
+const datalogin = {
+  email: "",
+  password: "",
+};
+
+// console.log(datalogin);
 
 export const LoginScrean = () => {
+  const [data, setData] = useState(datalogin);
+
+  const onDataLogin = (login, password) => {
+    console.log(login, password);
+  };
+
   return (
-    <View>
-      <Text>Hallo login</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../images/1.png")}
+        style={styles.image}
+      >
+        <View style={styles.innerBox}>
+          <Text style={styles.text}>Увійти</Text>
+          <TextInput
+            placeholder="login"
+            style={{ ...styles.input }}
+          ></TextInput>
+          <TextInput
+            // value={password}
+            style={{ ...styles.input }}
+            placeholder="Пароль"
+            // onChangeText={inputPassword}
+            secureTextEntry={true}
+            // onFocus={() => setisShowKeyboard(true)}
+          ></TextInput>
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.6}
+            // onPress={onLogin}
+          >
+            <Text style={styles.btnText}>Увійти</Text>
+          </TouchableOpacity>
+          <Text style={styles.registrText}>Немає акаунту? Зареєструватися</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
