be81216da97dd5d8bcfd3e6d2f0344de7ce20c88
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2023-08-28 23:03:49 +0300

    some add

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { useRouter } from "./screens/router";
 import { useState } from "react";
 
 export default function App() {
-  const [isAutch, setIsAutch] = useState(false);
+  const [isAutch, setIsAutch] = useState(true);
 
   const routing = useRouter(isAutch);
   // console.log(routing);
